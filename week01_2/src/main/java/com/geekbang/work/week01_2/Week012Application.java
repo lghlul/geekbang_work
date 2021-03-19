3e08f8f246eaf3902bb8d02ef00c4dd1ec9ecbf2
卢亮 <lgh_lul@163.com>
2021-03-19 09:01:38 +0800

    first commit

---
+++ week01_2/src/main/java/com/geekbang/work/week01_2/Week012Application.java
@@ -0,0 +1,13 @@
+package com.geekbang.work.week01_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week012Application.class, args);
+	}
+
+}
---
+++ week01_2/src/test/java/com/geekbang/work/week01_2/Week012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbang.work.week01_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

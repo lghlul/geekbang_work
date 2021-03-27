ebd7e9959276620159feb09dc8ca521dfef59f7c
卢亮 <lgh_lul@163.com>
2021-03-27 15:06:31 +0800

    week02_work

---
+++ week02_work6/src/main/java/com/example/week02_work6/Week02Work6Application.java
@@ -0,0 +1,13 @@
+package com.example.week02_work6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week02Work6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week02Work6Application.class, args);
+	}
+
+}
---
+++ week02_work6/src/test/java/com/example/week02_work6/Week02Work6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week02_work6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week02Work6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
